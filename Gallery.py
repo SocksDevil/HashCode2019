f4daff45dfe364381b7ef4e4164418f26c1bcf2d
Goncalo101 <goncalo.oli@outlook.com>
2019-02-28 18:12:01 +0000

    More attributes on Photo class

--- Gallery.py
+++ Gallery.py
@@ -4,9 +4,10 @@ class Gallery:
 
 
 class Photo:
-    def __init__(self, orientation, tags):
+    def __init__(self, orientation, num_tags, tags):
         self.orientation = orientation
+        self.num_tags = num_tags
         self.tags = tags
 
     def __repr__(self):
-        print((self.orientation, len(self.tags), self.tags.split()))
+        print((self.orientation, self.num_tags, self.tags.split()))
