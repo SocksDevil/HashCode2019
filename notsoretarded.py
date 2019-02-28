930aa95b218d7c8e7dd65f2d8d0163de3ebc6196
pdsam <paulodsam@gmail.com>
2019-02-28 21:20:48 +0000

    pure random

---
+++ notsoretarded.py
@@ -0,0 +1,29 @@
+import random
+
+
+f = open("file.txt", "w")
+counter = 0
+list=[]
+while counter < 80000:
+    list.append(counter)
+    print("generatin\n")
+    counter+=1
+
+f.write("40000\n")
+
+print(len(list))
+print("\n")
+while len(list) != 0:
+    r = random.randint(0,len(list)-1)
+    print(str(r)+ " " + str(len(list)) + "\n" )
+    if len(list) % 2 == 0:
+        f.write(str(list[r]) + " ")
+    else: 
+        if len(list) != 1:
+            f.write(str(list[r]) + "\n")
+        else:
+            f.write(str(list[r]))
+    list.pop(r)
+
+
+print(counter)
