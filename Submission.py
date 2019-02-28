49986fbe4ae9af5c8965ffdd1bf2960e3db53a4d
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-28 18:25:31 +0000

    Added submission and slide class

---
+++ Submission.py
@@ -0,0 +1,19 @@
+class Submission:
+    def __init__(self, slide_show):
+        self.slide_show = slide_show
+
+    def submit(self, filename):
+        f = open(filename, "w")
+        f.write(str(len(self.slide_show)))
+        f.write("\n")
+        for i in self.slide_show:
+            for j in i.IDs:
+                f.write(str(j))
+                f.write(" ")
+            f.write("\n")
+
+
+class Slide:
+    def __init__(self, IDs):
+        self.IDs = IDs
+        self.length = len(IDs)
