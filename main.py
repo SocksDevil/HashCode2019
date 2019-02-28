54fde4d75ecff97237602b4a0897424260ee0c40
GambuzX <mg_mesquita@hotmail.com>
2019-02-28 18:27:08 +0000

    Functions for array operations

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import numpy as np
+def binary_search(arr, item):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        middle = int((left + right) / 2)
+
+        if arr[middle] == item:
+            return True
+
+        if arr[middle] > item:
+            right = middle-1
+        else:
+            left = middle+1
+    return False
 
 
-var = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(var)
+def intersection(arr1, arr2):
+    return list(set(arr1) & set(arr2))
+
+
+def exclusive(arr1, arr2):
+    return list(set(arr1) - set(arr2))
+
+
+def calc_points(tags1, tags2):
+    return min(len(intersection(tags1, tags2)), len(exclusive(tags1,tags2)), len(exclusive(tags2, tags1)))
+
+
+
